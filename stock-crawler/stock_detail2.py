bc42682f1a0758a5d82a62015bc10fb0280202dc
lishihong <lieric7766@gmail.com>
2018-12-29 17:55:24 +0800

    reformat

--- stock-crawler/stock_detail2.py
+++ stock-crawler/stock_detail2.py
@@ -3,44 +3,40 @@ import os
 import json
 import twstock
 
-d=twstock.codes
+d = twstock.codes
 
-h={}
+h = {}
 
 for k in d:
-    info=d[k]
-    if info.type!="股票":continue
+    info = d[k]
+    if info.type != "股票": continue
     #print (k,info.name,info.type)
-    h[k]=info.name
+    h[k] = info.name
 
-
-
-s=open("stock.json").read()
-j=json.loads(s)
+s = open("stock.json").read()
+j = json.loads(s)
 
 for k in j:
     #print (k,j[k])
-    h[k]=j[k]
+    h[k] = j[k]
 
-
-i=0
-
-
-
+i = 0
 
 for k in h:
-    i+=1
-    print (i,len(h))
-    print (k,h[k])
-    print ("")
-    if os.path.isfile("cnyes/%s.html"%k):
-        print ('aaaa')
+    i += 1
+    print(i, len(h))
+    print(k, h[k])
+    print("")
+    if os.path.isfile("cnyes/%s.html" % k):
+        print('aaaa')
         continue
     #r=requests.post("http://pchome.megatime.com.tw/stock/sto0/ock3/sid%s.html"%k,data={"is_check":"1"})
     #r.encoding="utf8"
     while True:
-        s=os.popen("curl "+"https://www.cnyes.com/twstock/ps_pv_time/%s.htm"%k).read()
-        if len(s)>0:break
-    f=open("cnyes/%s.html"%k,"w")
+        s = os.popen(
+            "curl " +
+            "https://www.cnyes.com/twstock/ps_pv_time/%s.htm" % k).read()
+        if len(s) > 0: break
+    f = open("cnyes/%s.html" % k, "w")
     f.write(s)
     f.close()
